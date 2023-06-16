4ae9550aa87822156652f83279c8ae4716c0cc62
Elaidzha1940 <eli080293@gmail.com>
2023-06-17 00:00:08 +0300

    SwiftUI Scroll Animation

---
+++ SwiftUI Scroll Animation/SwiftUI_Scroll_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Scroll_AnimationApp.swift
+//  SwiftUI Scroll Animation
+//
+//  Created by Elaidzha Shchukin on 16.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Scroll_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
